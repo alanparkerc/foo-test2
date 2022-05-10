b5a55a28ab8f674799716054672e7284625aa9b4
Chase Parker <73307907+alanparkerc@users.noreply.github.com>
2022-05-09 23:44:29 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'foo-fighters-news.p.rapidapi.com',
+      'X-RapidAPI-Key': '4b95e0a240msh26e181aa8751bc0p18a5b2jsnf8a511ffe1e8'
+    }
+  };
+  
+  fetch('https://foo-fighters-news.p.rapidapi.com/news', options)
+    .then(response => response.json())
+    .then(response => appendData(response))
+    .catch(err => console.error(err));
+    function appendData(data) {
+      console.log(data);
+      var mainContainer = document.getElementById("myData");
+      for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = 'TITLE: ' + data[i].title + 'URL' + data[i].url;
+        mainContainer.appendChild(div);
+      }
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="myData"></div>
     </div>
   );
 }
